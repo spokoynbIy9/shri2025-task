4d321105d2cb0b1e779aadb0b01925bfcf97ce5f
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-02 16:42:11 +0500

    fix: put css files into src, delete base64 image, change png to webp

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client';
 import { Header } from './components/Header.jsx';
 import { MainContent } from './components/MainContent.jsx';
 
+import './reset.css';
+import './styles.css';
+
 createRoot(document.getElementById('app')).render(
 	<>
 		<Header />
