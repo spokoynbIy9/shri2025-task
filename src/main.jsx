84cd458f408cb442b841cd037743fef8156b087e
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-02 16:12:42 +0500

    fix: correct path, delete strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Header } from './components/Header.jsx';
 import { MainContent } from './components/MainContent.jsx';
 
 createRoot(document.getElementById('app')).render(
-	<StrictMode>
+	<>
 		<Header />
 		<MainContent />
-	</StrictMode>
+	</>
 );
